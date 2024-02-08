5f171cef2ce6cb6f799604ac16b1fd0f7c17df6b
Harshsharmma <hareshdev8827@gmail.com>
2024-02-08 19:19:48 +0530

    .m

--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -5,7 +5,7 @@ const CarList = ({ onClick }) => {
     {
       id: 1,
       name: "Hyundai Creta",
-      image: "./creata.jpg",
+      image: "./creata.JPG",
       price: 20000,
       mileage: 10000,
       engine: "1.5L Petrol",
@@ -13,7 +13,7 @@ const CarList = ({ onClick }) => {
     {
       id: 2,
       name: "Maruti Swift",
-      image: "./swift.jpg",
+      image: "./swift.JPG",
       price: 10000,
       mileage: 1000,
       engine: "2.5L Petrol",
@@ -21,7 +21,7 @@ const CarList = ({ onClick }) => {
     {
       id: 3,
       name: "Maruti Alto",
-      image: "./alto.jpg",
+      image: "./alto.JPG",
       price: 2000,
       mileage: 1000,
       engine: "1.5L Petrol",
